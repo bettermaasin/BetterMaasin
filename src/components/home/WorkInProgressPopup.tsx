51ca468efe6a14ccf78cf59a979f75e5313955c3
Derk Andre <67269555+derkandre@users.noreply.github.com>
2026-09-12 20:21:02 +0800

    feat(wip): update minor layout and content

--- src/components/home/WorkInProgressPopup.tsx
+++ src/components/home/WorkInProgressPopup.tsx
@@ -29,7 +29,7 @@ const WorkInProgressPopup: FC = () => {
 
   return (
     <Dialog open={open} onOpenChange={handleOpenChange}>
-      <DialogContent className='max-w-md max-h-[85vh] overflow-y-auto p-4 sm:p-6'>
+      <DialogContent className='max-w-md max-h-[85vh] overflow-y-auto p-5 sm:p-6'>
         <div className='pr-8'>
           <p className='text-[11px] font-semibold uppercase tracking-wider text-amber-600'>
             {t('workInProgress.notice')}
@@ -45,6 +45,25 @@ const WorkInProgressPopup: FC = () => {
 
         <div className='mt-2 rounded-md border border-amber-200 bg-amber-50 px-3 py-2 text-[13px] leading-snug text-amber-800'>
           {t('workInProgress.domainNotice')}
+        </div>
+
+        <div className='mt-3 border-t border-gray-100 pt-2 text-center text-xs text-gray-600'>
+          <p>
+            {t('workInProgress.agree')}{' '}
+            <Link
+              to='/terms-of-service'
+              className='text-primary-600 underline underline-offset-2 hover:text-primary-700'
+            >
+              {t('workInProgress.termsLink')}
+            </Link>{' '}
+            {t('workInProgress.and')}{' '}
+            <Link
+              to='/privacy-policy'
+              className='text-primary-600 underline underline-offset-2 hover:text-primary-700'
+            >
+              {t('workInProgress.privacyLink')}
+            </Link>
+          </p>
         </div>
 
         <div className='mt-4 flex flex-col-reverse gap-2 sm:flex-row'>
@@ -63,25 +82,6 @@ const WorkInProgressPopup: FC = () => {
           >
             {t('workInProgress.joinVolunteer')}
           </Link>
-        </div>
-
-        <div className='mt-4 border-t border-gray-100 pt-3 text-center text-xs text-gray-500'>
-          <p>
-            {t('workInProgress.agree')}{' '}
-            <Link
-              to='/terms-of-service'
-              className='text-primary-600 underline underline-offset-2 hover:text-primary-700'
-            >
-              {t('workInProgress.termsLink')}
-            </Link>{' '}
-            {t('workInProgress.and')}{' '}
-            <Link
-              to='/privacy-policy'
-              className='text-primary-600 underline underline-offset-2 hover:text-primary-700'
-            >
-              {t('workInProgress.privacyLink')}
-            </Link>
-          </p>
         </div>
       </DialogContent>
     </Dialog>
